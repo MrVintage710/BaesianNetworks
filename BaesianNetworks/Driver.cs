d1063be01e53058dfd104c4c0b7f7a18fd91dbe2
Kristoff <finley@kristoff.codes>
2021-10-27 08:42:09 -0600

    Created Classes to start from. Imported the provided evidence as .txt files for each model.
    Need to ask a question about the win95pts_evidence.txt -> The format is different then the others, Why? Does this change anything other than me having to parse differently?

---
+++ BaesianNetworks/Driver.cs
@@ -0,0 +1,22 @@
+namespace BaesianNetworks {
+    public class Driver {
+        //BaesNetwork Alarm;
+        //BaesNetwork Child
+        //BaesNetwork Hailfinder 
+        //BaesNetwork Insurance 
+        //BaesNetwork Win95pts 
+
+        public Driver() {
+            
+        }
+
+        // Uses Variable and Elimination on The models above calculate the 
+        // marginal distributions for the indicated variables
+        // TODO: Q: are we comparing the two algorithms? 
+        //      > they both perform inference, should they arrive at the same conclusion?
+        // TODO: Q: little vs moderate evidence for Win95pts Network?
+        private void Experiment() {
+            
+        }
+    }
+}
---
+++ BaesianNetworks/Evidence.cs
@@ -0,0 +1,11 @@
+namespace BaesianNetworks {
+    public class Evidence {
+        //List of Reports
+        //List of Little Evidence
+        //List of Moderate Evidence
+        //Fringe case for Win95pts Network
+        
+        /// Puts the evidence in the arguments above by parsing the .txt files with the evidence
+        //private void Parser() {}
+    }
+}
