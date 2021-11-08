7942f59bf3c1d2fdbd005695e552aa9dc5e302a5
Gearhartlove <finley@kristoff.codes>
2021-11-08 14:58:37 -0700

    progress

--- BaesianNetworks/VariableElimination/FactorTree.cs
+++ BaesianNetworks/VariableElimination/FactorTree.cs
@@ -10,48 +10,49 @@ namespace BaesianNetworks {
         
         public FactorTree(List<BaesNode> factors, BaesNetwork network) {
             this.network = network;
-            //parents = new Queue<FactorNode>();
-            //parents = new Que { };
-            //tree = new List<FactorNode>();
-            FactorNode master_node = new FactorNode("master");
-            ConstructPermutations(factors, master_node, 0 );
+            FactorNode startingNode = new FactorNode("start");
+            ConstructPermutations(factors, startingNode, 0);
         }
 
-        /// Construct the tree by recursively creating FactorNodes which link every permutation of a set of factors 
-        private void ConstructPermutations(List<BaesNode> factors, FactorNode parents, int count)  {
-            Console.WriteLine(count);
-            while (count < factors.Count) { // TODO might change to Count + 1 here
-                string considering = factors[count].getVariableName();
-                // There is a considered node in the baes network, and I am looping through every possible 
-                // variation of that variable
-                foreach (string value in network.getNode(considering).GetValues()) {
-                    FactorNode child = new FactorNode(considering + "=" + value);
-                    parents.AddChild(child);
-                    ConstructPermutations(factors, child, ++count);
-                }
-            }
-        }
+         // Construct the tree by recursively creating FactorNodes which link every permutation of a set of factors 
+         private void ConstructPermutations(List<BaesNode> factors, FactorNode parent, int count)  {
+              Console.WriteLine(count);
+              while (count < factors.Count) { // TODO might change to Count + 1 here
+                  string considering = factors[count].getVariableName();
+                  // There is a considered node in the baes network, and I am looping through every possible 
+                  // variation of that variable
+                  foreach (string value in network.getNode(considering).GetValues()) {
+                      FactorNode child = new FactorNode(considering + "=" + value);
+                      parent.AddChild(child);
+                      ConstructPermutations(factors, child, ++count);
+                  }
+                  //return ConstructPermutations(factors, parent, --count);
+              }
+         }
+         
+         
+        
     }
 
     class FactorNode {
         
         private FactorNode parent;
         private List<FactorNode> Children;
-        private string value;
+        private string value ;
+        // complexity is the total number of factors which makes up a probability combination
 
-        public FactorNode(string value = null) {
-            Children = new List<FactorNode>();
+        public FactorNode(string value) {
             this.value = value;
+            Children = new List<FactorNode>();
         }
-
-        public void SetParent(FactorNode parent) {
-            this.parent = parent;
-            parent.AddChild(this);
-        }
-
+        
         public void AddChild(FactorNode child) {
             Children.Add(child);
         }
+         private void SetParent(FactorNode parent) { 
+             this.parent = parent;
+             parent.AddChild(this);
+         }
 
     }
 }
